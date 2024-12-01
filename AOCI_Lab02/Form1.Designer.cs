2b40ec5a7e51ca1fe05cdbf730fff5ddc38b4f72
i.sologub <i.sologub@ADMINLAPTOP>
2024-12-01 12:20:15 +0700

    HSV, Sharp, Kontrast, Blur, add window filter with edit filter

--- AOCI_Lab02/Form1.Designer.cs
+++ AOCI_Lab02/Form1.Designer.cs
@@ -29,12 +29,239 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.DefaultImage = new Emgu.CV.UI.ImageBox();
+            this.ProcessImage = new Emgu.CV.UI.ImageBox();
+            this.OpenImageBtn = new System.Windows.Forms.Button();
+            this.ProcessBtn = new System.Windows.Forms.Button();
+            this.KontrastBtn = new System.Windows.Forms.Button();
+            this.HSVBtn = new System.Windows.Forms.Button();
+            this.HSVBar = new System.Windows.Forms.TrackBar();
+            this.BlurBtn = new System.Windows.Forms.Button();
+            this.SharpBtn = new System.Windows.Forms.Button();
+            this.FiltContrTxt = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt1 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt2 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt5 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt4 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt3 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt8 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt7 = new System.Windows.Forms.TextBox();
+            this.FiltContrTxt6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DefaultImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HSVBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DefaultImage
+            // 
+            this.DefaultImage.Location = new System.Drawing.Point(12, 12);
+            this.DefaultImage.Name = "DefaultImage";
+            this.DefaultImage.Size = new System.Drawing.Size(640, 480);
+            this.DefaultImage.TabIndex = 2;
+            this.DefaultImage.TabStop = false;
+            // 
+            // ProcessImage
+            // 
+            this.ProcessImage.Location = new System.Drawing.Point(658, 12);
+            this.ProcessImage.Name = "ProcessImage";
+            this.ProcessImage.Size = new System.Drawing.Size(640, 480);
+            this.ProcessImage.TabIndex = 3;
+            this.ProcessImage.TabStop = false;
+            // 
+            // OpenImageBtn
+            // 
+            this.OpenImageBtn.Location = new System.Drawing.Point(12, 499);
+            this.OpenImageBtn.Name = "OpenImageBtn";
+            this.OpenImageBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenImageBtn.TabIndex = 4;
+            this.OpenImageBtn.Text = "Open Image";
+            this.OpenImageBtn.UseVisualStyleBackColor = true;
+            this.OpenImageBtn.Click += new System.EventHandler(this.OpenImageBtn_Click);
+            // 
+            // ProcessBtn
+            // 
+            this.ProcessBtn.Location = new System.Drawing.Point(93, 499);
+            this.ProcessBtn.Name = "ProcessBtn";
+            this.ProcessBtn.Size = new System.Drawing.Size(75, 23);
+            this.ProcessBtn.TabIndex = 5;
+            this.ProcessBtn.Text = "Process";
+            this.ProcessBtn.UseVisualStyleBackColor = true;
+            this.ProcessBtn.Click += new System.EventHandler(this.ProcessBtn_Click);
+            // 
+            // KontrastBtn
+            // 
+            this.KontrastBtn.Location = new System.Drawing.Point(174, 499);
+            this.KontrastBtn.Name = "KontrastBtn";
+            this.KontrastBtn.Size = new System.Drawing.Size(75, 23);
+            this.KontrastBtn.TabIndex = 6;
+            this.KontrastBtn.Text = "Kontrast";
+            this.KontrastBtn.UseVisualStyleBackColor = true;
+            this.KontrastBtn.Click += new System.EventHandler(this.KontrastBtn_Click);
+            // 
+            // HSVBtn
+            // 
+            this.HSVBtn.Location = new System.Drawing.Point(255, 499);
+            this.HSVBtn.Name = "HSVBtn";
+            this.HSVBtn.Size = new System.Drawing.Size(75, 23);
+            this.HSVBtn.TabIndex = 7;
+            this.HSVBtn.Text = "HSV";
+            this.HSVBtn.UseVisualStyleBackColor = true;
+            this.HSVBtn.Click += new System.EventHandler(this.HSVBtn_Click);
+            // 
+            // HSVBar
+            // 
+            this.HSVBar.Location = new System.Drawing.Point(371, 499);
+            this.HSVBar.Maximum = 255;
+            this.HSVBar.Name = "HSVBar";
+            this.HSVBar.Size = new System.Drawing.Size(281, 45);
+            this.HSVBar.TabIndex = 8;
+            this.HSVBar.Scroll += new System.EventHandler(this.HSVBar_Scroll);
+            // 
+            // BlurBtn
+            // 
+            this.BlurBtn.Location = new System.Drawing.Point(255, 538);
+            this.BlurBtn.Name = "BlurBtn";
+            this.BlurBtn.Size = new System.Drawing.Size(75, 23);
+            this.BlurBtn.TabIndex = 9;
+            this.BlurBtn.Text = "Blur";
+            this.BlurBtn.UseVisualStyleBackColor = true;
+            this.BlurBtn.Click += new System.EventHandler(this.BlurBtn_Click);
+            // 
+            // SharpBtn
+            // 
+            this.SharpBtn.Location = new System.Drawing.Point(174, 538);
+            this.SharpBtn.Name = "SharpBtn";
+            this.SharpBtn.Size = new System.Drawing.Size(75, 23);
+            this.SharpBtn.TabIndex = 10;
+            this.SharpBtn.Text = "Sharp";
+            this.SharpBtn.UseVisualStyleBackColor = true;
+            this.SharpBtn.Click += new System.EventHandler(this.SharpBtn_Click);
+            // 
+            // FiltContrTxt
+            // 
+            this.FiltContrTxt.Location = new System.Drawing.Point(659, 499);
+            this.FiltContrTxt.MaxLength = 3;
+            this.FiltContrTxt.Name = "FiltContrTxt";
+            this.FiltContrTxt.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt.TabIndex = 11;
+            // 
+            // FiltContrTxt1
+            // 
+            this.FiltContrTxt1.Location = new System.Drawing.Point(701, 499);
+            this.FiltContrTxt1.MaxLength = 3;
+            this.FiltContrTxt1.Name = "FiltContrTxt1";
+            this.FiltContrTxt1.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt1.TabIndex = 12;
+            // 
+            // FiltContrTxt2
+            // 
+            this.FiltContrTxt2.Location = new System.Drawing.Point(743, 499);
+            this.FiltContrTxt2.MaxLength = 3;
+            this.FiltContrTxt2.Name = "FiltContrTxt2";
+            this.FiltContrTxt2.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt2.TabIndex = 13;
+            // 
+            // FiltContrTxt5
+            // 
+            this.FiltContrTxt5.Location = new System.Drawing.Point(743, 524);
+            this.FiltContrTxt5.MaxLength = 3;
+            this.FiltContrTxt5.Name = "FiltContrTxt5";
+            this.FiltContrTxt5.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt5.TabIndex = 16;
+            // 
+            // FiltContrTxt4
+            // 
+            this.FiltContrTxt4.Location = new System.Drawing.Point(701, 524);
+            this.FiltContrTxt4.MaxLength = 3;
+            this.FiltContrTxt4.Name = "FiltContrTxt4";
+            this.FiltContrTxt4.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt4.TabIndex = 15;
+            // 
+            // FiltContrTxt3
+            // 
+            this.FiltContrTxt3.Location = new System.Drawing.Point(659, 524);
+            this.FiltContrTxt3.MaxLength = 3;
+            this.FiltContrTxt3.Name = "FiltContrTxt3";
+            this.FiltContrTxt3.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt3.TabIndex = 14;
+            // 
+            // FiltContrTxt8
+            // 
+            this.FiltContrTxt8.Location = new System.Drawing.Point(743, 550);
+            this.FiltContrTxt8.MaxLength = 3;
+            this.FiltContrTxt8.Name = "FiltContrTxt8";
+            this.FiltContrTxt8.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt8.TabIndex = 19;
+            // 
+            // FiltContrTxt7
+            // 
+            this.FiltContrTxt7.Location = new System.Drawing.Point(701, 550);
+            this.FiltContrTxt7.MaxLength = 3;
+            this.FiltContrTxt7.Name = "FiltContrTxt7";
+            this.FiltContrTxt7.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt7.TabIndex = 18;
+            // 
+            // FiltContrTxt6
+            // 
+            this.FiltContrTxt6.Location = new System.Drawing.Point(659, 550);
+            this.FiltContrTxt6.MaxLength = 3;
+            this.FiltContrTxt6.Name = "FiltContrTxt6";
+            this.FiltContrTxt6.Size = new System.Drawing.Size(36, 20);
+            this.FiltContrTxt6.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1313, 627);
+            this.Controls.Add(this.FiltContrTxt8);
+            this.Controls.Add(this.FiltContrTxt7);
+            this.Controls.Add(this.FiltContrTxt6);
+            this.Controls.Add(this.FiltContrTxt5);
+            this.Controls.Add(this.FiltContrTxt4);
+            this.Controls.Add(this.FiltContrTxt3);
+            this.Controls.Add(this.FiltContrTxt2);
+            this.Controls.Add(this.FiltContrTxt1);
+            this.Controls.Add(this.FiltContrTxt);
+            this.Controls.Add(this.SharpBtn);
+            this.Controls.Add(this.BlurBtn);
+            this.Controls.Add(this.HSVBar);
+            this.Controls.Add(this.HSVBtn);
+            this.Controls.Add(this.KontrastBtn);
+            this.Controls.Add(this.ProcessBtn);
+            this.Controls.Add(this.OpenImageBtn);
+            this.Controls.Add(this.ProcessImage);
+            this.Controls.Add(this.DefaultImage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DefaultImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HSVBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox DefaultImage;
+        private Emgu.CV.UI.ImageBox ProcessImage;
+        private System.Windows.Forms.Button OpenImageBtn;
+        private System.Windows.Forms.Button ProcessBtn;
+        private System.Windows.Forms.Button KontrastBtn;
+        private System.Windows.Forms.Button HSVBtn;
+        private System.Windows.Forms.TrackBar HSVBar;
+        private System.Windows.Forms.Button BlurBtn;
+        private System.Windows.Forms.Button SharpBtn;
+        private System.Windows.Forms.TextBox FiltContrTxt;
+        private System.Windows.Forms.TextBox FiltContrTxt1;
+        private System.Windows.Forms.TextBox FiltContrTxt2;
+        private System.Windows.Forms.TextBox FiltContrTxt5;
+        private System.Windows.Forms.TextBox FiltContrTxt4;
+        private System.Windows.Forms.TextBox FiltContrTxt3;
+        private System.Windows.Forms.TextBox FiltContrTxt8;
+        private System.Windows.Forms.TextBox FiltContrTxt7;
+        private System.Windows.Forms.TextBox FiltContrTxt6;
     }
 }
 
--- AOCI_Lab02/Form1.cs
+++ AOCI_Lab02/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +10,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+using Emgu.CV.UI;
+using Emgu.CV.Util;
 
 namespace AOCI_Lab02
 {
@@ -15,6 +23,230 @@ namespace AOCI_Lab02
         public Form1()
         {
             InitializeComponent();
+            FiltContrTxt.Text = "-1";
+            FiltContrTxt1.Text = "-1";
+            FiltContrTxt2.Text = "-1";
+            FiltContrTxt3.Text = "-1";
+            FiltContrTxt4.Text = "9";
+            FiltContrTxt5.Text = "-1";
+            FiltContrTxt6.Text = "-1";
+            FiltContrTxt7.Text = "-1";
+            FiltContrTxt8.Text = "-1";
+        }
+
+        private Image<Bgr, byte> sourceImage; //глобальная переменная
+
+
+        //ProcessImage Secon ImageBox for 
+        private void OpenImageBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Файлы изображений (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+
+                if (result == DialogResult.OK) // открытие выбранного файла
+                {
+                    string fileName = openFileDialog.FileName;
+                    sourceImage = new Image<Bgr, byte>(fileName).Resize(640, 480, Inter.Linear);
+                    DefaultImage.Image = sourceImage;//.Resize(640, 480, Inter.Linear)
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ProcessBtn_Click(object sender, EventArgs e)
+        {
+            OperationColor(sourceImage.Convert<Gray, byte>());
+        }
+
+
+        private void OperationColor(Image<Gray, byte> grayImage)
+        {
+            try
+            {
+                var channel = sourceImage.Split()[0];
+
+                Image<Bgr, byte> destImage = sourceImage.CopyBlank();
+                
+
+
+                VectorOfMat vm = new VectorOfMat();
+                vm.Push(channel); 
+                vm.Push(channel.CopyBlank());
+                vm.Push(channel.CopyBlank());
+                CvInvoke.Merge(vm, destImage);
+
+
+                ProcessImage.Image = destImage;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void KontrastBtn_Click(object sender, EventArgs e)
+        {
+            var contrastImage = sourceImage.Copy();
+            int c = 2;
+            for (int y = 0; y < contrastImage.Height; y++)
+            {
+                for (int x = 0; x < contrastImage.Width; x++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if ((contrastImage.Data[y, x, i] * c) > 255)
+                        {
+                            contrastImage.Data[y, x, i] = 255;
+                        }
+                        else
+                            contrastImage.Data[y, x, i] = Convert.ToByte(contrastImage.Data[y, x, i] * c);
+
+                    }
+                    //contrastImage.Data[y, x, 0] = Convert.ToByte(contrastImage.Data[y, x, 0] * c);
+                    //contrastImage.Data[y, x, 1] = Convert.ToByte(contrastImage.Data[y, x, 1] * c);
+                    //contrastImage.Data[y, x, 2] = Convert.ToByte(contrastImage.Data[y, x, 2] * c);
+                }
+
+            }
+
+
+
+            ProcessImage.Image = contrastImage.Resize(640, 480, Inter.Linear);
+        }
+
+        private void HSVBtn_Click(object sender, EventArgs e)
+        {
+            //Image<Hsv,byte>
+            var hsvImage = sourceImage.Convert<Hsv, byte>();
+
+            for (int y = 0; y < hsvImage.Height; y++)
+            {
+                for (int x = 0; x < hsvImage.Width; x++)
+                {
+                    hsvImage.Data[y, x, 0] = Convert.ToByte(HSVBar.Value);
+
+                }
+            }
+
+
+            ProcessImage.Image = hsvImage.Resize(640, 480, Inter.Linear);
+        }
+
+        private void HSVBar_Scroll(object sender, EventArgs e)
+        {
+            var hsvImage = sourceImage.Convert<Hsv, byte>();
+
+            for (int y = 0; y < hsvImage.Height; y++)
+            {
+                for (int x = 0; x < hsvImage.Width; x++)
+                {
+                    hsvImage.Data[y, x, 0] = Convert.ToByte(HSVBar.Value);
+
+                }
+            }
+
+            ProcessImage.Image = hsvImage.Resize(640, 480, Inter.Linear);
+        }
+
+        private void BlurBtn_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> gray = sourceImage.Convert<Gray, byte>();
+            Image<Gray, byte> result = gray.CopyBlank();
+            
+            int sh = 1;
+            int N = 3;
+
+            List<byte> l = new List<byte>();
+
+
+            for (int y = sh; y < gray.Height-sh; y++)
+            {
+                for (int x = sh; x < gray.Width-sh; x++)
+                {
+                    l.Clear();
+                    
+                    for (int i = -1; i < 2; i++)
+                    {
+                        for (int j = -1; j < 2; j++)
+                        { 
+                            l.Add(gray.Data[i+x, j+y, 0]);
+                        }
+                    }
+
+                    l.Sort();
+
+                    result.Data[y, x, 0] = l[N / 2];//l[(1+sh*2)];
+                    //gray.Data[y, x, 0] = Convert.ToByte(HSVBar.Value);
+
+                }
+            }
+
+            DefaultImage.Image = gray;//.Resize(640, 480, Inter.Linear);
+
+            ProcessImage.Image = result;//.Resize(640, 480, Inter.Linear);
+        }
+
+        private void SharpBtn_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> gray = sourceImage.Convert<Gray, byte>();
+            Image<Gray, byte> result = gray.CopyBlank();
+
+            int[,] w = new int[3, 3] 
+            {
+               {Convert.ToInt32(FiltContrTxt.Text), Convert.ToInt32(FiltContrTxt1.Text), Convert.ToInt32(FiltContrTxt2.Text)},
+               {Convert.ToInt32(FiltContrTxt3.Text), Convert.ToInt32(FiltContrTxt4.Text), Convert.ToInt32(FiltContrTxt5.Text)},
+               {Convert.ToInt32(FiltContrTxt6.Text), Convert.ToInt32(FiltContrTxt7.Text), Convert.ToInt32(FiltContrTxt8.Text)},
+            };
+
+
+            for (int y = 1; y < gray.Height - 1; y++)
+            {
+                for (int x = 1; x < gray.Width - 1; x++)
+                {
+                    int r = 0;
+                    for (int i = -1; i < 2; i++)
+                    {
+                        for (int j = -1; j < 2; j++)
+                        {
+                            r += gray.Data[i + y, j + x, 0] * w[i + 1, j + 1];
+                        }
+                    }
+
+                    if (r > 255)
+                        r = 255;
+                    if (r < 0) 
+                        r = 0;
+
+                    result.Data[y, x, 0] = (byte)r;
+                    //gray.Data[y, x, 0] = Convert.ToByte(HSVBar.Value);
+
+                }
+            }
+
+            DefaultImage.Image = gray;//.Resize(640, 480, Inter.Linear);
+
+            ProcessImage.Image = result;//.Resize(640, 480, Inter.Linear);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
